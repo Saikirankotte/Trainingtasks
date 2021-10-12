ace794081596e7c8d7f772d2317cd76ea84435a3
saikirankotte <49676152+Saikirankotte@users.noreply.github.com>
2021-10-12 13:24:19 +0530

    Add files via upload

---
+++ Data/ContactRepository.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using System.Data.SqlClient;
+using System.Data;
+using Dapper.Contrib.Extensions;
+using System.Data.SqlClient;
+
+
+
+namespace Data
+{
+    public class ContactRepository : IContactRepository
+    {
+        private System.Data.IDbConnection db;
+
+
+
+        public ContactRepository(string connString)
+        {
+            this.db = new SqlConnection(connString);
+        }
+
+
+        public Train Add(Train train)
+        {
+            this.db.Insert(train);
+            return train;
+        }
+    }
+}
